f03f08527ef9ab2ec215e1e5ec2abba3d9c65d4b
MacBook_lilac <1412735542@qq.com>
2026-02-20 23:02:26 +0800

    add get_capacity

--- moe_utils.py
+++ moe_utils.py
@@ -1,5 +1,29 @@
+from typing import Optional
+import math
+
 import torch
 from torch import Tensor
+
+
+def get_capacity(
+    num_tokens: int, num_experts: int, capacity_factor: float, min_capacity: Optional[int] = None
+) -> int:
+    """
+    Calculate the capacity of each expert.
+
+    Args:
+        num_tokens: num of the input tokens.
+        num_experts: num of the experts.
+        capacity_factor: capacity factor.
+        min_capacity: minimum capacity, default to None.
+    
+    Returns:
+        capacity of each expert.
+    """
+    capacity = math.ceil((num_tokens / num_experts) * capacity_factor)
+    if min_capacity is not None and capacity < min_capacity:
+        capacity = min_capacity
+    return capacity
 
 
 def maybe_move_tensor_to_cpu(
