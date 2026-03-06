152e13f0f3e182fa98b4e91053c2be5b2de6dce9
MacBook_lilac <1412735542@qq.com>
2026-03-06 23:01:45 +0800

    define group of pgs

--- parallel_state.py
+++ parallel_state.py
@@ -1,1 +1,33 @@
+import logging
+from typing import Optional, List, Callable
+from math import log2
+import einops
+
 import torch
+
+from moe_utils import GlobalMemoryBuffer
+
+
+# Intra-layer model parallel group that the current rank belongs to.
+_TENSOR_MODEL_PARALLEL_GROUP = None
+# Inter-layer model parallel group that the current rank belongs to.
+_PIPELINE_MODEL_PARALLEL_GROUP = None
+# Model parallel group(both intra- and pipeline) that the current rank belongs to.
+_MODEL_PARALLEL_GROUP = None
+# Context parallel group that the current rank belongs to.
+_CONTEXT_PARALLEL_GROUP = None
+# Expert parallel group that the current rank belongs to.
+_EXPERT_MODEL_PARALLEL_GROUP = None
+# Expert tensor parallel group that the current rank belongs to.
+_EXPERT_TENSOR_PARALLEL_GROUP = None
+# Tensor and expert parallel group that the current rank belongs to.
+_TENSOR_AND_EXPERT_PARALLEL_GROUP = None
+# Data parallel group that the current rank belongs to.
+_DATA_PARALLEL_GROUP = None
+# Expert data parallel group that the current rank belongs to.
+_EXPERT_DATA_PARALLEL_GROUP = None
+
+# _EXPERT_MODEL denotes expert parallelism which splits the number of experts across the group
+# _EXPERT_TENSOR denotes tensor parallelism of expert which parameters across the group
+# _EXPERT_DATA denotes data parallelism of expert which replicates weight across the group
+
