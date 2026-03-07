1dc652234c887239cb217104e4ec0a53a0280c55
MacBook_lilac <1412735542@qq.com>
2026-03-07 22:02:29 +0800

    define  for process group

--- parallel_state.py
+++ parallel_state.py
@@ -31,3 +31,32 @@ _EXPERT_DATA_PARALLEL_GROUP = None
 # _EXPERT_TENSOR denotes tensor parallelism of expert which parameters across the group
 # _EXPERT_DATA denotes data parallelism of expert which replicates weight across the group
 
+# A list of global ranks for each tensor model parallel group to ease calculation of the
+# first local rank in the tensor model parallel group.
+_TENSOR_MODEL_PARALLEL_GLOBAL_RANKS = None
+
+# A list of global ranks for each pipeline model parallel group to ease calculation of the
+# source rank when broadcasting from the first or last pipeline stage.
+_PIPELINE_MODEL_PARALLEL_GLOBAL_RANKS = None
+
+# A list of global ranks for each model parallel group to ease calculation of the
+# first local rank in the model parallel group.
+_MODEL_PARALLEL_GLOBAL_RANKS = None
+
+# A list of global ranks for each context parallel group to ease calculation of the
+# destination rank when exchanging KV/dKV between context parallel ranks.
+_CONTEXT_PARALLEL_GLOBAL_RANKS = None
+
+# A list of global ranks for each expert parallel group to ease calculation of the
+# first local rank in the expert parallel group.
+_EXPERT_MODEL_PARALLEL_GLOBAL_RANKS = None
+
+# A list of global ranks for each expert tensor parallel group to ease calculation of the
+# source rank when broadcasting weights from src to all other data parallel ranks.
+_DATA_PARALLEL_GLOBAL_RANKS = None
+
+# List of all process groups
+# Used for updating the timeout for all process groups
+# None represents the default process group
+_global_process_group_list = None
+
